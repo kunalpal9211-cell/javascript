10dd03274916c77e9c7ad6e86832b2cf6a75b354
Kunal <kunalpal9211@gmail.com>
2026-05-16 13:15:36 +0000

    datatypes

---
+++ basics/conversionOperations.js
@@ -0,0 +1,20 @@
+let score = 33;
+let score2 = null;
+let score3 = "33";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+console.log(typeof score2);
+console.log(typeof(score2));
+
+console.log(typeof score3);
+console.log(typeof(score3));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "333abc" => Nan
+// true => 1: false => 0;
---
+++ basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 14457
+let accounntEmail = "kunalpal92111@gmail.com"
+var accountPass = "1234"
+accountCity = "delhi"
+
+//dont use var instead use let 
+//accountId = 2
+console.log(accountPass);
+console.table([accountId, accounntEmail, accountPass])
