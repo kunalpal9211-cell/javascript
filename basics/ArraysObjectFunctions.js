c6499da2e799e4651ab6ff252e00f275d3295223
Kunal <kunalpal9211@gmail.com>
2026-05-16 14:19:56 +0000

    Non Primitive Basics

---
+++ basics/ArraysObjectFunctions.js
@@ -0,0 +1,16 @@
+// Arrays, Objects, Functions
+
+const heros = ["IronMan", "Thor", "Captain America"];
+
+let myObj = {
+    Name : "Kunal",
+    age : 20,
+};
+
+const myFunct = function() {
+    console.log("Hello World");
+}
+
+myFunct();
+let ans = myFunct();
+console.log(ans);
