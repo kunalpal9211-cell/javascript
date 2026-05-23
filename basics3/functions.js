10ee1550611fb201e8ca75539535267a32d5e508
Kunal <kunalpal9211@gmail.com>
2026-05-23 08:28:37 +0000

    functions

--- basics3/functions.js
+++ basics3/functions.js
@@ -21,3 +21,47 @@ function addNum(Num1, Num2) {
 
 const ans = addNum(3, 4);
 console.log(ans);
+
+function loginUserMsg(username) {
+    if (!username) {
+        console.log("Enter a username");
+        return
+    }
+    return `${username} just logged in!`;
+}
+
+console.log(loginUserMsg("Kunal"));
+console.log(loginUserMsg());
+
+
+function calculateCartPrice(...num1) {
+    return num1;
+}
+
+console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "Kunal",
+    price: 199
+}
+
+function handleObject(anyObject) {
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+handleObject(user);
+
+handleObject({
+    username: "Sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600];
+
+function returnSecondValue(getArray) {
+    return getArray[1];
+}
+
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
+
