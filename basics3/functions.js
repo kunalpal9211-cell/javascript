da5cc663b6a87f527f0e312859897ef1617aa68a
Kunal <kunalpal9211@gmail.com>
2026-05-23 08:28:22 +0000

    arrays

---
+++ basics3/functions.js
@@ -0,0 +1,23 @@
+
+
+function sayMyName() {
+    console.log("K");
+    console.log("U");
+    console.log("N");
+    console.log("A");
+    console.log("L");
+}
+
+// sayMyName();
+
+function addNum(Num1, Num2) {
+    console.log(Num1 + Num2);
+}
+
+function addNum(Num1, Num2) {
+    let ans = Num1 + Num2;
+    return ans;
+}
+
+const ans = addNum(3, 4);
+console.log(ans);
