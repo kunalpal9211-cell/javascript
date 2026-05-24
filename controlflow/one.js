2775dcb9fa4a568a2e7f29bb26657687f5d5c7b4
Kunal <kunalpal9211@gmail.com>
2026-05-24 11:45:43 +0000

    loops

---
+++ controlflow/one.js
@@ -0,0 +1,19 @@
+// if (condition) {
+   // console.log("Hello!");
+ //}
+
+const isUserLoggedIn = true;
+
+if (isUserLoggedIn) {
+    console.log("Kunal");
+}
+
+if (2 == '2') {
+    console.log("executed");
+} 
+
+// it checks data type also
+
+if (2 === '2') {
+    console.log("not executed");
+}
